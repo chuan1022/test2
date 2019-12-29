a5bf23e90e46a234941809862f73f1b659bcd60a
yuchuan <825077792@qq.com>
2019-12-29 21:31:43 +0800

    build

--- src/components/button.js
+++ src/components/button.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 
 class Button extends Component {
 
-  static defatultProps = {
-    size: 20,
-    color: "#333"
-  };
-
   render() {
     const { color, children, size } = this.props;
     return (
